04ce2e1074958840dfc7883aecccc57bf8b16838
mx <max@maxvl.com>
2023-06-23 20:26:01 -0400

    Reopen file, read from file and deserialize back into Movement struct

--- serde_json_bb2/src/main.rs
+++ serde_json_bb2/src/main.rs
@@ -1,7 +1,9 @@
 use serde::{Serialize, Deserialize};
 use std::fs::File;
 use std::io::Result;
+use std::io::BufReader;
 use std::io::Write;
+use std::io::Read;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Movement {
@@ -9,18 +11,25 @@ struct Movement {
 }
 
 fn main()  -> Result<()> {
+    // create movement struct
     let a = Movement { num_steps: 3 };
+
+    // create file
     let mut file = File::create("move.bin")?;
+
+    // serialize struct and write to file
     let serialized = serde_json::to_string(&a)?;
     file.write(serialized.as_bytes())?;
 
-    // TODO: figure out how to read the contents of the file back in and deserialize that as a
-    // Movement struct as the below is not doing that what you want
-    let deserialized: Movement = serde_json::from_str(&serialized)?;
+    // reopen file, read from file and deserialize back into struct
+    let open_file = File::open("move.bin")?;
+    let mut buf_reader = BufReader::new(open_file);
+    let mut contents = String::new();
 
-    
+    buf_reader.read_to_string(&mut contents)?;
 
-
+    let movement: Movement = serde_json::from_str(&contents)?;
+    println!("{:?}", movement);
 
     Ok(())
     
