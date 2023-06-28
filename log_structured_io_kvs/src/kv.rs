78aef97ea7635144b0daddf5ccb20a7c20332d9d
mx <max@maxvl.com>
2023-06-27 22:56:42 -0400

    start basics of KvStore struct

--- log_structured_io_kvs/src/kv.rs
+++ log_structured_io_kvs/src/kv.rs
@@ -1,4 +1,23 @@
 #[allow(dead_code)]
+use std::path::Path;
 pub struct KvStore {
     stuff: i32,
+}
+
+impl KvStore {
+    pub fn open(path: &Path) -> std::io::Result<()> {
+        panic!()
+    }
+
+    pub fn set(key: String, val: String) -> std::io::Result<()> {
+        panic!()
+    }
+
+    pub fn get(key: String) -> std::io::Result<()> {
+        panic!()
+    }
+
+    pub fn remove(key: String) -> std::io::Result<()> {
+        panic!()
+    }
 }
