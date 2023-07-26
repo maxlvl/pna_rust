3cd0eea59e537659ccf0cc3de1a4a0c18fe76f9c
mx <max@maxvl.com>
2023-07-25 23:04:31 -0400

    add basic imports for kvs

--- log_structured_io_kvs/src/bin/kvs.rs
+++ log_structured_io_kvs/src/bin/kvs.rs
@@ -1,1 +1,2 @@
+use std::result::Result;
 fn main() {}
--- log_structured_io_kvs/src/lib.rs
+++ log_structured_io_kvs/src/lib.rs
@@ -1,8 +1,8 @@
 // #![deny(missing_docs)]
 //! A simple key/value store.
 
-//pub use error::{KvsError, Result};
+pub use error::{KvsError, Result};
 pub use kv::KvStore;
 
-// mod error;
+mod error;
 mod kv;
