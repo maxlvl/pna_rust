8a55fc97f26a32697f62c2b58692b5ab7c0cab5f
mx <max@maxvl.com>
2023-07-26 23:16:15 -0400

    implemented std::io::Error for KvsError

--- log_structured_io_kvs/src/error.rs
+++ log_structured_io_kvs/src/error.rs
@@ -1,3 +1,12 @@
-pub enum KvsError {}
+use std::io;
+#[derive(Debug)]
+pub enum KvsError {
+    Io(io::Error),
+}
 
+impl From<io::Error> for KvsError {
+    fn from(err: io::Error) -> KvsError {
+        KvsError::Io(err)
+    }
+}
 pub type Result<T> = std::result::Result<T, KvsError>;
